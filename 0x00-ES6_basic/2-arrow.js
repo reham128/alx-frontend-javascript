126886692aa8ba3b0e20b635f197fe6c8f29b6da
Reham <r_nour30@yahoo.com>
2024-07-23 01:01:47 +0000

    task 2

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,9 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
+  const self = this;
   this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
+    return self.sanFranciscoNeighborhoods;
   };
 }
