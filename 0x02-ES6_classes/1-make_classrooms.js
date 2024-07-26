f93608bc32f3b89a9da88bfbe6cab7289273c767
Reham <r_nour30@yahoo.com>
2024-07-26 02:58:49 +0000

    up

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,8 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
   const sizes = [19, 20, 34];
-  return sizes.map(size => new ClassRoom(size));
+  return sizes.map((size) => new ClassRoom(size));
 }
 
 export default initializeRooms;
